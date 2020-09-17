7e01f27994fb2da027e0f5efa55bb224cbdb3772
Amey Thakur <amey36thakur@gmail.com>
2020-09-17 18:07:04 +0530

    Option of capturing images through camera is added

---
+++ AndroidStudioProjects/DSC Templates/Notes Maker/Android_1_NotesMaker/app/src/main/java/com/example/notesmaker/NotesActivity.java
@@ -0,0 +1,153 @@
+package com.example.notesmaker;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.getbase.floatingactionbutton.FloatingActionButton;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+public class NotesActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE_STORAGE_PERMISSION = 1;
+    private static final int REQUEST_CODE_SELECT_IMAGE = 1;
+    private static final int REQUEST_CODE_SELECT_DOC = 10;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notes);
+
+        textView = findViewById(R.id.preview_text);
+
+        FloatingActionButton camFab = findViewById(R.id.fab_cam);
+        camFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // amey let ur code go here...
+
+                {
+                    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                    }
+                }
+
+            }
+        });
+
+        FloatingActionButton imgFab = findViewById(R.id.fab_image);
+        imgFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(
+                            NotesActivity.this,
+                            new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                            REQUEST_CODE_STORAGE_PERMISSION
+                    );
+                }
+                else
+                {
+                    selectImage();
+                }
+            }
+        });
+
+        FloatingActionButton docFab = findViewById(R.id.fab_doc);
+        docFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myFileIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                myFileIntent.setType("*/*");
+                startActivityForResult(myFileIntent, REQUEST_CODE_SELECT_DOC);
+            }
+        });
+    }
+
+    private void selectImage() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        if(intent.resolveActivity(getPackageManager())!=null)
+        {
+            startActivityForResult(intent, REQUEST_CODE_SELECT_IMAGE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(requestCode == REQUEST_CODE_STORAGE_PERMISSION && grantResults.length>0)
+        {
+            selectImage();
+        }
+        else
+        {
+            Toast.makeText(this, "Permission Denied!!!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            getTextFromBitmap(imageBitmap);
+        }
+
+
+        if (requestCode == REQUEST_CODE_SELECT_IMAGE && resultCode == RESULT_OK) {
+            if (data != null) {
+
+                Uri selectedImageUri = data.getData();
+                if (selectedImageUri != null) {
+                    InputStream inputStream = null;
+                    try {
+                        inputStream = getContentResolver().openInputStream(selectedImageUri);
+                        Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+//                        varun u can get the bitmap from here for ocr...
+
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                        Toast.makeText(this, "Exception" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+
+            }
+        }
+
+        if(requestCode == REQUEST_CODE_SELECT_DOC && resultCode == RESULT_OK){
+            // doc code will go here ...
+        }
+
+    }
+
+    private void getTextFromBitmap(Bitmap imageBitmap) {
+        //Enter your code from here Varun
+    }
+
+    private void previewText(String string){
+        textView.setText(string);
+    }
+}
