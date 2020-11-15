d1059d569a3938914b4b9958b71ba39f20aeb237
AdityaHonrao <aditya.honrao19@vit.edu>
2020-11-15 11:18:47 +0530

    Added Image Crop and Retrieve

--- app/src/main/java/com/example/notesmaker/Register.java
+++ app/src/main/java/com/example/notesmaker/Register.java
@@ -1,32 +1,41 @@
 package com.example.notesmaker;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.FirebaseException;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.PhoneAuthCredential;
-import com.google.firebase.auth.PhoneAuthProvider;
-import com.rilixtech.widget.countrycodepicker.CountryCodePicker;
-import java.util.concurrent.TimeUnit;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.yalantis.ucrop.UCrop;
+
+import java.io.File;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class Register extends AppCompatActivity {
+    private static final int SELECT_PICTURE = 0;
+    public static final int PIC_CROP = 1;
     EditText rCodeEnter, rEmailId, rPassword, rName;
+    CircleImageView profilePicture;
+    Uri pfp; //profile Picture
     FirebaseAuth fAuth;
     Button Next;
     private static final String TAG = "Register";
@@ -35,12 +44,18 @@ public class Register extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_register);
         fAuth = FirebaseAuth.getInstance();
         Next = findViewById(R.id.button_next);
         rEmailId = findViewById(R.id.edit_text_emailId);
         rPassword = findViewById(R.id.edit_text_password);
         rName = findViewById(R.id.edit_text_name);
+        profilePicture = findViewById(R.id.profile_image);
+
+        profilePicture.setOnClickListener(new SelectImage());
+        pfp = null;
+
 
         Next.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -66,18 +81,20 @@ public class Register extends AppCompatActivity {
                 }
 
 
-
                 fAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             FirebaseUser user = fAuth.getCurrentUser();
-                            user.sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>() {
-                                                                                  @Override
-                                                                                  public void onSuccess(Void aVoid) {
-                                                                                      Toast.makeText(Register.this, "Verification Message has been sent", Toast.LENGTH_LONG).show();
-                                                                                  }
-                                                                              }
+                            user.sendEmailVerification()
+                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                        @Override
+                                        public void onSuccess(Void aVoid) {
+                                            Toast.makeText(Register.this, "Verification Message has been sent", Toast.LENGTH_LONG).show();
+
+
+                                        }
+                                    }
                             ).addOnFailureListener(new OnFailureListener() {
                                 @Override
                                 public void onFailure(@NonNull Exception e) {
@@ -95,6 +112,41 @@ public class Register extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), MainActivity.class));
             }
         });
+    }
+
+    public class SelectImage implements View.OnClickListener{
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent();
+            intent.setType("image/*");
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+
+            startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_PICTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK){
+            if (requestCode == SELECT_PICTURE){
+                Uri image = data.getData();
+
+                CropImage.activity(image)
+                        .start(this);
+            }
+
+
+            if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+                CropImage.ActivityResult result = CropImage.getActivityResult(data);
+                if (resultCode == RESULT_OK) {
+                    pfp = result.getUri();
+                    profilePicture.setImageURI(pfp);
+                } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                    Exception error = result.getError();
+                }
+            }
+        }
     }
 }
 
